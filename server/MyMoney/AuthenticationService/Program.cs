7b927ffaf2871d739bfdc12239d8005ed452c9f9
Dori Lahav Waisberg <dori.lahav@gmail.com>
2024-03-02 17:58:57 +0200

    refactor: convert services Program.cs to top level statements

--- server/MyMoney/AuthenticationService/Program.cs
+++ server/MyMoney/AuthenticationService/Program.cs
@@ -1,20 +1,27 @@
-using Microsoft.AspNetCore.Hosting;
+using Authentication.DependencyInjection;
+using AuthenticationService.Utils;
+using Communication.Http;
+using DataAccess.Mongo;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace AuthenticationService
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddMongoConnection("mongodb://localhost:27017/my-money-auth-service");
+builder.Services.AddJwtServiceAuthentication("AuthService");
+builder.Services.AddHttpClient<HttpCommunication>();
+builder.Services.AddSingleton<ICodeGenerator, StaticCodeGenerator>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseRouting();
+app.MapControllers();
+app.Run();
--- server/MyMoney/UserService/Program.cs
+++ server/MyMoney/UserService/Program.cs
@@ -1,20 +1,39 @@
-using Microsoft.AspNetCore.Hosting;
+using Authentication.DependencyInjection;
+using Caching.Memcached;
+using DataAccess.Mongo;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace UserService
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddJwtClientAuthentication();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ServiceOnly", policy => policy.RequireClaim("type", "service"));
+    options.AddPolicy("UserOnly", policy => policy.RequireClaim("type", "user"));
+});
+
+builder.Services.AddMongoConnection("mongodb://localhost:27017/my-money-user-service");
+builder.Services.AddMemcachedCache(options =>
+{
+    options.AddServer("localhost", 11211);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
